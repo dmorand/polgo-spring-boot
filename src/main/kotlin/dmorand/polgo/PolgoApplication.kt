7db9a2c83b31fd668c578da7ed74e3d61ae27617
David Morand <david.morand@gmail.com>
2018-09-12 12:06:26 -0400

    Initialized project with Spring Initializr (https://start.spring.io/)

---
+++ src/main/kotlin/dmorand/polgo/PolgoApplication.kt
@@ -0,0 +1,11 @@
+package dmorand.polgo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolgoApplication
+
+fun main(args: Array<String>) {
+    runApplication<PolgoApplication>(*args)
+}
